60df8092de2ff5bec6b71084504bba8a5989047d
mjaqamy <mjaqamy@gmail.com>
2026-01-20 22:15:35 +0300

    feat: fix new aboutme

--- aboutme.ts
+++ aboutme.ts
@@ -1,7 +1,5 @@
 export const name = "Юлия";
 export const course = "автоматизации тестирования на JS";
 export const age = 28;
-const a = {
-  1: 123,
-  2: "234",
-};
+const a = { 1: 123, 
+    2: "234" };
